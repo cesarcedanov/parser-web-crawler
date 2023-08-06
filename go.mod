9b144a435c2d929cd62b33fbbfaf28c524f370c6
Cesar Cedano Velez <cesar.velez.consultant@sothebys.com>
2023-08-06 16:04:18 -0400

    Updated the Script to be able to Crawl a default Link and Collect the others links (and show up to verify it works)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/cesarcedanov/challenges/Parser/parser-web-crawler
 
 go 1.19
+
+require golang.org/x/net v0.14.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"time"
+)
+
+const parserURL = "https://parserdigital.com/"
 
 func main() {
 	fmt.Println("Hello Parser!")
+
+	inspectURLContent(parserURL)
+}
+
+// initClient will create our Client to do requests
+//
+// timeout is 100 secs
+func initClient() *http.Client {
+	return &http.Client{
+		Timeout: 100 * time.Second,
+	}
+}
+
+// getContentFromURL get the URLs content
+func getContentFromURL(url string) (*http.Response, error) {
+	client := initClient()
+
+	response, err := client.Get(url)
+	if err != nil {
+		fmt.Printf("Error while getting the Content from URL (%s) due to: %s", url, err)
+		return nil, err
+	}
+	return response, nil
+}
+
+// inspectURLContent will connect to the URL
+// and inspect the HTML Content to extract link
+func inspectURLContent(url string) {
+	response, err := getContentFromURL(url)
+	if err != nil {
+		fmt.Printf("%s - Error: %s", "inpectURLContent", err)
+		return
+	}
+
+	defer response.Body.Close()
+
+	z := html.NewTokenizer(response.Body)
+	for {
+		tokenType := z.Next()
+		if tokenType == html.ErrorToken {
+			return
+		}
+
+		token := z.Token()
+		var links []string
+		if isStartAnchorTag(token, tokenType) {
+			link := extractLinksFromTag(token)
+			links = append(links, link)
+			// Append into the queue of link
+			// Then send it to the Channel to Crawl them too
+			fmt.Println(link)
+		}
+
+	}
+}
+
+// isStartAnchorTag return true when the token is <a>
+func isStartAnchorTag(token html.Token, tokenType html.TokenType) bool {
+	//  We need the <a> token instead of the </a>, because those have the href="" attr
+	return tokenType == html.StartTagToken && token.DataAtom.String() == "a"
+}
+
+// extractLinksFromTag get the href value from the Tag
+func extractLinksFromTag(token html.Token) string {
+	for _, attr := range token.Attr {
+		if attr.Key == "href" {
+			return attr.Val
+		}
+	}
+	return ""
 }
